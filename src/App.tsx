522ed83dbc47462393c1b84418a66bccf37e30d3
bill-go-huynh <bill.huynh.gos@gmail.com>
2025-03-05 22:23:47 +0700

    Chore: add SASS support and improve ProfileCard accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './App.css'
+import './App.scss'
 import ProfileCard from './components/ProfileCard'
+import React from 'react'
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         name="Minh Thai"
         position="Fullstack Developer"
         avatarUrl="https://media-hosting.imagekit.io//2071d947ae2a4cd3/aThai.jpg?Expires=1835767252&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=nGjSLjlzWZQA4mN290PYjsrn8y~CaC6Ooe1wNB7rR01wLI474kq3G~An2jyuvAw19dQK6QcSw-5NYeBGaDrWvBbJppgd4uo765piZ4usWtQPL8kCFx9OFGTHhkhbTH5n6URUQcuYE4I2AN1ompPb1~09YCqrTHSTknjjQMX1Cu~QsaM-64VwAC8NJfAkzR5sg6zobuHmtLtqiVaSjga7le9psBDtwpzlZnNv0FGkARn7lNwFHAOMFal5H~eGZS3ubq2s-Hoefs4F6Bm-uaSeKHVN2fz1obJAfTPQyL6B9TOrsRDO6EXu~oBkAERW3LSTjBVrYJ0yYOQ8Q2UfgnvFVg__"
-        facebookUrl="https://facebook.com"
+        facebookUrl="https://www.facebook.com/minhthai.hoang.3"
         youtubeUrl="https://youtube.com"
         tiktokUrl="https://tiktok.com"
         githubUrl="https://github.com"
@@ -29,7 +29,7 @@ function App() {
         name="Cuong Nv"
         position="Fullstack Developer"
         avatarUrl="https://media-hosting.imagekit.io//5f6845fed61043cd/Cuong.jpg?Expires=1835767252&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=0mBz0Ejju868H-yEziT411o3PjaeCPtUXWsBRnxxtgxsgkoBEa5t1KxOopdtPz7A23ZKf~18VP3iQNdwhLPWczmAy614QzZxhx6XUtEuOfxxcz2WcQxh0i-vydoeqlhMJaCMGi36jbWSL7nZGaOfRYOlxfTQIo9x~gvvBu~iswquCruu6baCmZj5hYlzPIkVTvWorxBczgLDznp-j1qp3xNprP4g8SBcjmNGzrp6P9QNyM5Jm8AlbzwvKj62VjXZm4ohQjp3pUB4tmel1BI6MymfaEFcf5jo6pJtjpxZXi~k-P4bCoWCLIo7kh5xUBAYiD9O6z~zrCMlSdGl6ENYVw__"
-        facebookUrl="https://facebook.com"
+        facebookUrl="https://www.facebook.com/CuongNv.2002/"
         youtubeUrl="https://youtube.com"
         tiktokUrl="https://tiktok.com"
         githubUrl="https://github.com"
